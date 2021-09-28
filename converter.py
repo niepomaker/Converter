7e69688a4813daab8221d8b10c9f0ef8560da83a
niepomaker <paprotlukasz@gmail.com>
2021-09-28 08:39:36 +0000

    save JSON to file

--- converter.py
+++ converter.py
@@ -2,10 +2,19 @@ import json, os
 
 os.system("wget --certificate=/usr/share/n6/roman.osinski-dzp.pl.pem --private-key=/usr/share/n6/roman.osinski.n6.key https://n6beta.cert.pl/report/threats.json -P /home/lukasz/Converter/Converter")
 
+def saveToFile(i):
+    file_txt = open(i['category']  + '.txt', "a")
+    counter = 0
+    if ('address' in i):
+        while (counter < len(i['address'])):
+            print(i['address'][counter]["ip"])
+            file_txt.write(i['address'][counter]["ip"] + '\n')
+            counter = counter + 1
 
 file_json = open('threats.json')
 data = json.load(file_json)
 for i in data:
+    saveToFile(i)
     print(i)
 
 
