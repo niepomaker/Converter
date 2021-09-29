77d343878265fbe3620fba8462414f596c397561
niepomaker <paprotlukasz@gmail.com>
2021-09-29 17:04:38 +0000

    disposal duplicates

--- converter.py
+++ converter.py
@@ -1,7 +1,11 @@
+import datetime
 import json, os
 import psycopg2
+from datetime import date
+import datetime
+from datetime import datetime as xd
 
-os.system("wget --certificate=/usr/share/n6/roman.osinski-dzp.pl.pem --private-key=/usr/share/n6/roman.osinski.n6.key https://n6beta.cert.pl/report/threats.json -P /home/lukasz/Converter/Converter")
+# os.system("wget --certificate=/usr/share/n6/roman.osinski-dzp.pl.pem --private-key=/usr/share/n6/roman.osinski.n6.key https://n6beta.cert.pl/report/threats.json -P /home/lukasz/Converter/Converter")
 
 connection = psycopg2.connect(user="postgres", password="JeBJh4tELW9nq4Ohk9VA", host="localhost",
                               port="5432", database="roman")
@@ -16,7 +20,8 @@ def saveToFile(i):
             file_txt.write(i['address'][counter]["ip"] + '\n')
             counter = counter + 1
 
-def base(id, category, confidance, name, url, time, source, cc, ip, asn, fqdn, block, sha256, dport, target):
+
+def writeToBase(id, category, confidance, name, url, time, source, cc, ip, asn, fqdn, block, sha256, dport, target):
 
     postgres_insert_query = """ INSERT INTO json (id, category, confidence, name, url, time, source, cc, ip, asn, fqdn, block, sha256, dport, target) 
                                 VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"""
@@ -26,13 +31,20 @@ def base(id, category, confidance, name, url, time, source, cc, ip, asn, fqdn, b
 
     connection.commit()
 
+def readIdFromBase():
+    allId = []
+    postgres_insert_query = """SELECT id FROM json"""
+    cursor.execute(postgres_insert_query)
+    data = cursor.fetchall()
+    for row in data:
+        allId.append(row[0])
+    return allId
 
 def saveToFile(i):
     file_txt = open(i['category'] + '.txt', "a")
     counter = 0
     if ('address' in i):
         while (counter < len(i['address'])):
-            print(i['address'][counter]["ip"])
             file_txt.write(i['address'][counter]["ip"] + '\n')
             counter = counter + 1
 
@@ -46,13 +58,12 @@ def saveToBaseWithAddress(i, keys, counter):
         keys[j] = i['address'][counter][j]
 
     if (counter == 0):
-        base(keys["id"], keys["category"], keys["confidence"], keys["name"], keys["url"], keys["time"], keys["source"],
+        writeToBase(keys["id"], keys["category"], keys["confidence"], keys["name"], keys["url"], keys["time"], keys["source"],
              keys["cc"], keys["ip"], keys["asn"], keys['fqdn'], keys['block'],
              keys['sha256'], keys['dport'], keys['target'])
     else:
-        base(keys["id"] + "/" + str(counter), keys["category"], keys["confidence"], keys["name"], keys["url"],
-             keys["time"], keys["source"],
-             keys["cc"], keys["ip"], keys["asn"], keys['fqdn'], keys['block'],
+        writeToBase(keys["id"] + "/" + str(counter), keys["category"], keys["confidence"], keys["name"], keys["url"],
+             keys["time"], keys["source"], keys["cc"], keys["ip"], keys["asn"], keys['fqdn'], keys['block'],
              keys['sha256'], keys['dport'], keys['target'])
 
 
@@ -61,7 +72,7 @@ def saveToBaseWithoutAddress(i, keys):
         if key in i.keys():
             keys[key] = i[key]
 
-    base(keys["id"], keys["category"], keys["confidence"], keys["name"], keys["url"], keys["time"], keys["source"],
+    writeToBase(keys["id"], keys["category"], keys["confidence"], keys["name"], keys["url"], keys["time"], keys["source"],
          keys["cc"], keys["ip"], keys["asn"], keys['fqdn'], keys['block'],
          keys['sha256'], keys['dport'], keys['target'])
 
@@ -81,18 +92,46 @@ def newEmptyDictionary(data):
 file_json = open('threats.json')
 data = json.load(file_json)
 
+# for i in data:
+#     counter = 0
+#     dataId = readIdFromBase()
+#     if i['id'] not in dataId:
+#         if 'address' in i:
+#             while(len(i['address']) > counter):
+#                 saveToBaseWithAddress(i, newEmptyDictionary(data), counter)
+#                 counter += 1
+#         else:
+#             saveToBaseWithoutAddress(i, newEmptyDictionary(data))
+
+print("JSON został dodany do bazy danych oraz zostały utworzone pliki .txt")
+
+counter = 0
+counter1 = 0
+
+def takeAllRecords():
+    postgres_insert_query = """SELECT * FROM json"""
+    cursor.execute(postgres_insert_query)
+    data = cursor.fetchall()
+    return data
+
 for i in data:
-    saveToFile(i)
-    counter = 0
-    if 'address' in i:
-        while(len(i['address']) > counter):
-            saveToBaseWithAddress(i, newEmptyDictionary(data), counter)
-            counter += 1
-    else:
-        saveToBaseWithoutAddress(i, newEmptyDictionary(data))
+    counter1 += 1
+    date0 = i['time'].replace("T", " ").split(" ")[0]
+    date1 = xd.strptime(date0, '%Y-%m-%d')
 
+    today = date.today() - datetime.timedelta(days=1)
+    today1 = today.strftime("%Y-%m-%d")
 
+    currentDay = xd.strptime(date0, '%Y-%m-%d')
+    weekAgo = xd.strptime(today1, '%Y-%m-%d')
+    if currentDay >= weekAgo:
+        counter += 1
+        # saveToFile(i)
+
+print(counter)
+print(counter1)
 file_json.close()
+# os.remove("/home/lukasz/Converter/Converter/threats.json")
 if connection:
     cursor.close()
     connection.close()
